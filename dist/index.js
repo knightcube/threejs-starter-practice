1f0fb9ce2807f266f1d9423d1e026e96f58503d0
Rajat Kumar Gupta <rajatcube@gmail.com>
2021-04-01 18:34:52 +0530

    Updated static link in index.js

--- dist/index.js
+++ dist/index.js
@@ -6,7 +6,7 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 const textureLoader = new THREE.TextureLoader()
-const myTexture = textureLoader.load('../static/coolTex.jpg')
+const myTexture = textureLoader.load('https://github.com/knightcube/threejs-starter-practice/blob/gh-pages/static/coolTex.jpg')
 
 // Object
 const geometry = new THREE.BoxGeometry(1,1,1)
